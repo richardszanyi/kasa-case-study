dc203cf35ab316934457c30d44eee73dd13adeb7
Richard Szanyi <szanyirichard94@gmail.com>
2022-01-16 17:25:56 +0100

    - Working e2e implementation - Reorder feature file folder structure - TODO: Refactor step definitions

--- cypress/integration/properties.feature
+++ cypress/integration/properties.feature
@@ -2,8 +2,8 @@ Feature: Properties can be searched on the home page. Single night stay is not a
 
   Scenario Outline: Search Kasas in different locations
     Given I am on the home page
-    When I set the following fields in the search bar <city>, <numberOfNights> nights and <numberOfGuests> guest
-    And I click the "Search" button
+    When I set the following fields in the search bar <city>, <numberOfNights> nights and <numberOfGuests> guests
+    And I click the "Search" button in the Search Widget
     Then Search results contains <city>
     And Calculations are for <numberOfNights> nights and <numberOfGuests> guest
     Examples:
@@ -15,17 +15,17 @@ Feature: Properties can be searched on the home page. Single night stay is not a
 
   Scenario: Check if the system allows guests to book a single night stay and bookings can only be made on a future date.
     Given I am on the home page
-    When I set the following fields in the search bar "Austin, TX", 1 night and 1 guest
-    And I click the "Search" button
+    When I set the following fields in the search bar "Austin, TX", 1 nights and 2 guests
+    And I click the "Search" button in the Search Widget
     Then Search results contains "Austin, TX"
     And Warning is displayed regarding minimum night stays
 
 
   Scenario: Amenities shows up on the property details page appropriately
     Given I am on the home page
-    When I set the following fields in the search bar "Austin, TX", 1 night and 1 guest
-    And I click the "Search" button
+    When I set the following fields in the search bar "Austin, TX", 2 nights and 2 guests
+    And I click the "Search" button in the Search Widget
     Then Search results contains "Austin, TX"
-    When I click on the first properties' "View Details" button
+    When I click on the first properties' "View details" button
     Then Property details page is opened
     And Amenities "Basics" table contains "Heating"
--- cypress/support/step_definitions/commonSteps.ts
+++ cypress/support/step_definitions/commonSteps.ts
@@ -1,3 +1,94 @@
 /// <reference types="Cypress" />
 
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+const getIframeDocument = () => {
+  return cy
+    .get('iframe[title="Modal Message"]')
+    .its('0.contentDocument').should('exist')
+};
+
+const getIframeBody = () => {
+  return getIframeDocument()
+    .its('body').should('not.be.undefined')
+    .then(cy.wrap)
+};
+
+Given(`I am on the home page`, () => {
+
+  cy.visit("/");
+  cy.get(".search-widget").should("be.visible");
+
+  // wait for the modal asking for email address to be visible and then close it
+  cy.get(".ab-iam-root").should("be.visible");
+  getIframeBody().find('.ab-close-button').click();
+});
+
+When('I set the following fields in the search bar {string}, {int} nights and {int} guests',
+  (destination: string, nights: number, guests: number) => {
+
+    const tomorrow: Date = new Date()
+    const endDateOfAccommodation: Date = new Date();
+    const today: Date = new Date();
+
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    endDateOfAccommodation.setDate(tomorrow.getDate() + nights);
+
+    const todayFormatted: string = today.toISOString().slice(0, 10);
+    const tomorrowFormatted: string = tomorrow.toISOString().slice(0, 10);
+    const endDateOfAccommodationFormatted: string = endDateOfAccommodation.toISOString().slice(0, 10);
+
+    cy.get('input[id="full-screen-hero-search-input"]').type(destination);
+
+    cy.get('input[id="full-screen-hero-check-in-input"]').click({ force: true, scrollBehavior: false });
+
+    cy.get(`button[date="${todayFormatted}"]`).should('have.attr', 'disabled', 'disabled');
+
+    cy.get(`button[date="${tomorrowFormatted}"]`).click({ force: true, scrollBehavior: false });
+
+    cy.get('input[id="full-screen-hero-check-out-input"]').click({ force: true, scrollBehavior: false });
+    cy.get(`button[date="${endDateOfAccommodationFormatted}"]`).click({ force: true, scrollBehavior: false });
+
+    cy.get('.guest-count-input__btn').click({ force: true, scrollBehavior: false });
+    cy.get('input[id="full-screen-hero-adult-guest-count-input"]').click({ force: true, scrollBehavior: false }).type('{backspace}').type(guests.toString());
+    cy.get('span').contains("Close").click({ force: true, scrollBehavior: false });
+    cy.scrollTo(0, 0);
+
+  });
+
+When('I click the {string} button in the Search Widget', (buttonName: string) => {
+  cy.get('.search-widget button').contains(buttonName).click({ force: true, scrollBehavior: false });
+});
+
+Then('Search results contains {string}', (text: string) => {
+  cy.get('h1').should('contain.text', text);
+  const propertyTable = cy.get('.list-page-properties > .list-page__content-list').find('li');
+  propertyTable.should('be.visible');
+  propertyTable.find('span').should('have.length.at.least', 7)
+});
+
+Then('Calculations are for {int} nights and {int} guest', (numberOfNights: number, numberOfGuests: number) => {
+  cy.get('.list-page-properties > .list-page__content-list').find('li').filter('.property-card').each(($li) => {
+    cy.wrap($li).find('.recommended-room-type__price-details__guest-and-night').should('contain.text', `${numberOfNights} nights, ${numberOfGuests} guests`)
+  })
+});
+
+Then('Warning is displayed regarding minimum night stays', () => {
+  cy.get('.list-page-properties > .list-page__content-list').find('li').filter('.property-card').each(($li) => {
+    cy.wrap($li).contains('Minimum 2-night stays at this property.').should('be.visible');
+  })
+});
+
+When('I click on the first properties\' {string} button', (buttonName: string) => {
+  cy.get('.list-page-properties > .list-page__content-list').find('li').filter('.property-card').first()
+    .find('.text-container').contains(buttonName).click();
+});
+
+Then('Property details page is opened', () => {
+  cy.get('.room-type-popup__card').should('be.visible');
+});
+
+Then('Amenities {string} table contains {string}', (tableName: string, amenityName: string) => {
+  const amenity = cy.get('.room-type-popup__card').find('h4').contains(tableName).siblings('ul').find('li').contains(amenityName);
+  amenity.scrollIntoView();
+  amenity.should('be.visible');
+})
